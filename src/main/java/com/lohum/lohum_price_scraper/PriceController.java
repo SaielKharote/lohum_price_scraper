c0fc0f7654e87fc6ff79acd602703fad98017c81
Saiel Kharote <saielkharote219@gmail.com>
2023-04-28 22:50:01 +0530

    controller working fine

--- src/main/java/com/lohum/lohum_price_scraper/PriceController.java
+++ src/main/java/com/lohum/lohum_price_scraper/PriceController.java
@@ -1,7 +1,25 @@
 package com.lohum.lohum_price_scraper;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 public class PriceController {
+
+    @GetMapping("/price")
+    public String getPrice() {
+        String url = "https://www.metal.com/Lithium-ion-Battery/202303240001";
+        try {
+            Document doc = Jsoup.connect(url).get();
+            String price = doc.selectFirst(".priceDown___2TbRQ").text();
+            return price;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Error found";
+        }
+    }
 }

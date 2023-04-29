860e9fbc7220773ada9bf47a7ddfdd36cd952ef1
Saiel Kharote <saielkharote219@gmail.com>
2023-04-29 10:36:23 +0530

    handled errors

--- src/main/java/com/lohum/lohum_price_scraper/PriceController.java
+++ src/main/java/com/lohum/lohum_price_scraper/PriceController.java
@@ -1,5 +1,6 @@
 package com.lohum.lohum_price_scraper;
 
+import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,15 +12,14 @@ import java.io.IOException;
 public class PriceController {
 
     @GetMapping("/price")
-    public String getPrice() {
+    public String getPrice() throws IOException {
         String url = "https://www.metal.com/Lithium-ion-Battery/202303240001";
         try {
             Document doc = Jsoup.connect(url).get();
             String price = doc.selectFirst(".priceDown___2TbRQ").text();
             return price;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return "Error found";
+        } catch (Exception error) {
+            return "Encountered an error while fetching the price. " + "(Error: "+ error.getMessage()+")";
         }
     }
 }
